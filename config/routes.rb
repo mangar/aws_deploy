05cc4616fb3df377bc07cff73ad2bd05edd5475e
Marcio Mangar <marcio.mangar@gmail.com>
2013-10-31 16:25:36 -0200

    only displaying for development and staging environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do  
-  namespace :aws_deploy do 
-    root :to => 'index#index', :as => :root
-    match "/send_to_production"  => "index#send_to_production", :as => :send_to_production, via: [:get]
+  if Rails.env == "development" || Rails.env == "staging"
+    namespace :aws_deploy do 
+      root :to => 'index#index', :as => :root
+      match "/send_to_production"  => "index#send_to_production", :as => :send_to_production, via: [:post]
+    end
   end
 end
