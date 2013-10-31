9762a2f44609a77af0ca82832988c28787d3e75c
Marcio Mangar <marcio.mangar@gmail.com>
2013-10-31 16:59:48 -0200

    sycn

--- config/initializers/aws_deploy.rb
+++ config/initializers/aws_deploy.rb
@@ -2,8 +2,9 @@
 
   puts "=> AWS Deploy loaded!"
 
-  if File.exist?(Rails.root.join("config/aws_deploy.yml"))
-    settings = YAML.load(File.read(File.expand_path(Rails.root.join("config/aws_deploy.yml"), File.dirname(__FILE__))))
+  if File.exist?(File.expand_path('../../config/aws_deploy.yml', File.dirname(__FILE__)))
+
+    settings = YAML.load(File.read(File.expand_path('../../config/aws_deploy.yml', File.dirname(__FILE__))))
     env = "#{ENV['RAILS_ENV']}".blank? ? "development" : "#{ENV['RAILS_ENV']}"
     $deployer_mg = settings["#{env}"]
     
