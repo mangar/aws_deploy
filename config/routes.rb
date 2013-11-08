4ea8a38255eef1f2627d3125710b9f1e046a1a5b
Marcio Mangar <marcio.mangar@gmail.com>
2013-11-08 11:34:49 -0200

    new environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do  
-  if Rails.env == "development" || Rails.env == "staging"
+  if Rails.env == "development" || Rails.env == "staging" || Rails.env == "testrga"
     namespace :aws_deploy do 
       root :to => 'index#index', :as => :root_awsdeploy
       match "/send_to_production"  => "index#send_to_production", :as => :send_to_production_awsdeploy, via: [:post]
