3974451bfd8b5d4ac915f9d70f0867aa4f3b66d0
Marcio Mangar <marcio.mangar@gmail.com>
2013-11-08 08:41:45 -0200

    v.0.1.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do  
   if Rails.env == "development" || Rails.env == "staging"
     namespace :aws_deploy do 
-      root :to => 'home#index', :as => :root_awsdeploy
+      root :to => 'index#index', :as => :root_awsdeploy
       match "/send_to_production"  => "index#send_to_production", :as => :send_to_production_awsdeploy, via: [:post]
     end
   end
--- lib/aws_deploy/version.rb
+++ lib/aws_deploy/version.rb
@@ -1,3 +1,3 @@
 module AwsDeploy
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
