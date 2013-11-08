5ee8ab8d7d5129592e90f31f8d0a4b6c0af304d2
Marcio Mangar <marcio.mangar@gmail.com>
2013-11-08 08:39:37 -0200

    path on route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do  
   if Rails.env == "development" || Rails.env == "staging"
     namespace :aws_deploy do 
-      root :to => 'index#index', :as => :root
-      match "/send_to_production"  => "index#send_to_production", :as => :send_to_production, via: [:post]
+      root :to => 'home#index', :as => :root_awsdeploy
+      match "/send_to_production"  => "index#send_to_production", :as => :send_to_production_awsdeploy, via: [:post]
     end
   end
 end
+
+
+
+
+
