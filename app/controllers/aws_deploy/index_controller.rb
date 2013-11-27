844814bb0bcef98258c759d619f94811267e0961
Marcio Mangar <marcio.mangar@gmail.com>
2013-11-27 16:36:37 -0200

    development_remote

--- app/controllers/aws_deploy/index_controller.rb
+++ app/controllers/aws_deploy/index_controller.rb
@@ -40,20 +40,20 @@ class AwsDeploy::IndexController < AwsDeploy::AwsDeployApplicationController
       s3 = AWS::S3.new
       bucket = s3.buckets["#{_aws_bucket}"]
 
-      if bucket.objects[_md5_file("test")].exists?
+      if bucket.objects[_md5_file("development_remote")].exists?
 
           # remove files in production...
           bucket.objects[_md5_file("#{environment}")].delete if bucket.objects[_md5_file("#{environment}")].exists?
           bucket.objects[_zip_file("#{environment}")].delete if bucket.objects[_zip_file("#{environment}")].exists?
 
           # copying files from test to production
-          bucket.objects[_md5_file("test")].copy_to(_md5_file("#{environment}"))
-          bucket.objects[_zip_file("test")].copy_to(_zip_file("#{environment}"))
+          bucket.objects[_md5_file("development_remote")].copy_to(_md5_file("#{environment}"))
+          bucket.objects[_zip_file("development_remote")].copy_to(_zip_file("#{environment}"))
 
           message += " (#{environment})"
 
       else
-        message = "Test file not found. #{_md5_file("test")}"
+        message = "DevelopmentRemote file not found. #{_md5_file("development_remote")}"
       end
 
     else 
