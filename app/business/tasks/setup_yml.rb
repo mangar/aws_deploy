d6bb5507b5690adb72ae0ae27cadc27cdfe21586
Marcio Mangar <marcio.mangar@gmail.com>
2013-12-06 11:00:18 -0200

    Fixed rake task aws:setup. Now generating the aws_deploy.yml with the right parameters

--- app/business/tasks/setup_yml.rb
+++ app/business/tasks/setup_yml.rb
@@ -16,16 +16,16 @@ class Tasks::SetupYml
     # aws_deploy.yml
 
     content = "development:\n" + 
-"  aws_bucket: \"directory/deploy\" \n" + 
-"  aws_access_key_id: \"YOUR_ACCESS_KEY_ID__HERE or ENV['YOUR_KEY_EXPORTED']\" \n" + 
-"  aws_secret_access_key: \"YOUR_SECRET_ACCESS_KEY__HERE or ENV['YOUR_KEY_EXPORTED']\" \n" + 
-"  file_pattern: \"FILE_NAME\" \n" + 
+"  aws_bucket: \"#{@aws_deploy_bucket}\" \n" + 
+"  aws_access_key_id: \"#{@aws_access_key_id}\" \n" + 
+"  aws_secret_access_key: \"#{@aws_secret_access_key}\" \n" + 
+"  file_pattern: \"#{@file_pattern}\" \n" + 
 "\n" + 
 "development_remote: \n" + 
-"  aws_bucket: \"directory/deploy\"\n" + 
-"  aws_access_key_id: \"YOUR_ACCESS_KEY_ID__HERE or ENV['YOUR_KEY_EXPORTED']\"\n" + 
-"  aws_secret_access_key: \"YOUR_SECRET_ACCESS_KEY__HERE or ENV['YOUR_KEY_EXPORTED']\"\n" + 
-"  file_pattern: \"FILE_NAME\"\n"
+"  aws_bucket: \"#{@aws_deploy_bucket}\"\n" + 
+"  aws_access_key_id: \"#{@aws_access_key_id}\"\n" + 
+"  aws_secret_access_key: \"#{@aws_secret_access_key}\"\n" + 
+"  file_pattern: \"#{@file_pattern}\"\n"
 
     File.delete("#{Rails.root}/config/aws_deploy.yml") if File.exists?("#{Rails.root}/config/aws_deploy.yml")
 
@@ -40,27 +40,26 @@ class Tasks::SetupYml
   end
 
 
+  # 
+  # 
   def collect_inputs
     
     print "What is the of your AWS BUCKET ? [directory/deploy] :"
     @aws_deploy_bucket = STDIN.gets 
     @aws_deploy_bucket = (@aws_deploy_bucket.blank? ? "directory/deploy" : @aws_deploy_bucket)
-    puts "#{@aws_deploy_bucket} \n"
+    puts "#{@aws_deploy_bucket.gsub!("\n","")} \n"
 
     print "What is your AWS ACCESS KEY ID ? [my_key_id] :"
     @aws_access_key_id = STDIN.gets || "my_key_id"
-    puts "#{@aws_access_key_id} \n"
+    puts "#{@aws_access_key_id.gsub!("\n","")} \n"
 
     print "What is your AWS SECRET ACCESS KEY ? [my_secret_key] :"
     @aws_secret_access_key = STDIN.gets || "my_secret_key"
-    puts "#{@aws_secret_access_key} \n"
+    puts "#{@aws_secret_access_key.gsub!("\n","")} \n"
 
     print "What is your FILE PATERN ? [my_file_before_zip_ext] :"
     @file_pattern = STDIN.gets || "my_file_before_zip_ext"
-    puts "#{@file_pattern} \n"
-
-
-
+    puts "#{@file_pattern.gsub!("\n","")} \n"
 
   end
 
