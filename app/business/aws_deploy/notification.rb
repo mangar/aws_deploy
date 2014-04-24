6da57000cb4e690c8f8dfe233c8ab7651e4de754
Marcio Mangar <marcio.mangar@gmail.com>
2014-04-24 18:37:15 -0300

    Extra info about the notification

---
+++ app/business/aws_deploy/notification.rb
@@ -0,0 +1,75 @@
+class AwsDeploy::Notification
+
+
+  # 
+  # 
+  # 
+  def self.send to="all"
+    @errors = []
+
+    if to == "all"
+
+      api_keys = $aws_deploy["pushbullet"]["api_key"]
+      keys = api_keys.split(" ")
+
+      keys.each_with_index do |k,i| 
+
+      end
+  <li>
+    <%= label_tag "item_#{i}", k do%>
+      <%= check_box_tag "item_#{i}", k, :checked %>
+      <%= k%>
+    <% end %>
+  </li>
+  <% end %>
+
+
+
+
+
+
+
+
+      begin
+        _sendn(vals[0], vals[1])
+      rescue Exception => e
+        @errors << "Problem sending to: #{vals[0]}|#{vals[1]}|#{vals[2]} ... #{e}"
+      end
+
+    else
+
+
+    end
+
+    @errors
+  end
+
+
+
+  # 
+  # 
+  # 
+  def _sendn token, device_name
+    push = MyPush.new :token => token
+
+    # find the device...
+    iden = ""
+    devices = push.devices
+    devices["devices"].each do |dev|
+      if device_name == dev["extras"]["model"]
+        puts "ID: #{dev["iden"]}"
+        iden = dev["iden"]
+      end
+    end
+
+    # send a message..
+    push.push :type => MyPush::PUSH_TYPE_NOTE, 
+          :title => "[TEST][aws_deploy] #{Rails.application.class.parent_name} #{Rails.env}", 
+          :body => "This is a test message sent from aws_deploy", 
+          :iden => "#{iden}"
+
+  end
+
+
+
+end
